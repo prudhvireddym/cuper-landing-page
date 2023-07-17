fd25b4045fb39161cabd6632abe1f5d264f8394c
prudhvireddym <prudhvireddy.m@gmail.com>
2023-07-17 00:15:05 -0500

    able to click signup

--- components/ChatWidget.tsx
+++ components/ChatWidget.tsx
@@ -1,10 +1,10 @@
 function ChatWidget() {
     return (
-        <div style={{ position: 'fixed', bottom: '10px', right: '10px', zIndex: 1000 }}>
+        <div style={{ position: 'fixed', bottom: '10px', right: '10px', zIndex: 1000, width: '550px', height: '750px', overflow: 'hidden' }}>
             <iframe
                 src="https://cuper-chat.vercel.app/"
-                width="650"
-                height="850"
+                width="100%"
+                height="100%"
                 style={{ border: 'none' }}
             />
         </div>
